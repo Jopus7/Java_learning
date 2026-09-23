9dbeb5e481312d9e3edb7b62bab5b93916d7c3df
Jopus7 <micha.jopa@gmail.com>
2026-09-23 19:50:05 +0200

    added swing components

--- src/lekcja01/swing/SwingTest.java
+++ src/lekcja01/swing/SwingTest.java
@@ -1,8 +1,6 @@
 package lekcja01.swing;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.*;
+
 
 public class SwingTest {
 
@@ -12,16 +10,49 @@ public class SwingTest {
         okno.setTitle("Java Swing");
         okno.setSize(600, 400);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton przycisk = new JButton("Kliknij mnie");
-        okno.add(przycisk);
-        okno.setVisible(true);
+        JPanel panel = new JPanel();
+        okno.add(panel);
 
-        przycisk.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Kliknięto przycisk!");
+        JLabel etykieta = new JLabel("Podaj swoje imię:");
+        panel.add(etykieta);
+
+        JTextField poleTekstowe = new JTextField(20);
+        panel.add(poleTekstowe);
+
+        JButton przycisk = new JButton("Kliknij mnie");
+        panel.add(przycisk);
+
+        JCheckBox prawoJazdy = new JCheckBox("Mam prawo jazdy");
+        panel.add(prawoJazdy);
+
+
+
+        przycisk.addActionListener(e -> {
+
+            if( poleTekstowe.getText().trim().isEmpty()){
+                JOptionPane.showMessageDialog(okno, "Podaj imie!");
+            }
+            else {
+                if(prawoJazdy.isSelected()){
+                    JOptionPane.showMessageDialog(okno, "masz prawo jazdy ");
+
+                }
+                else {
+                    JOptionPane.showMessageDialog(okno, "Nie masz prawka ");
+
+                }
+//            etykieta.setText("Cześć, " + poleTekstowe.getText() + "!");
+                JOptionPane.showMessageDialog(okno, "Cześć! " + poleTekstowe.getText());
             }
         });
+
+
+
+
+
+
+        okno.setVisible(true);
+
 
 
     }
